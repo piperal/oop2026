42d54963385f82563b538e19b0156d9237529551
piperal <piperal@tlu.ee>
2026-02-21 12:34:50 +0200

    initial commit

---
+++ 03_20.02-klassikomplekt/sportlasetulemused/sportlasetulemused/src/test/java/ee/piperal/sportlasetulemused/SportlasetulemusedApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.piperal.sportlasetulemused;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportlasetulemusedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
