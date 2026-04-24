01c246c77b69705acf1a8e36bf2294ceea977958
vahermihkel <65609256+vahermihkel@users.noreply.github.com>
2026-04-24 13:20:32 +0300

    payment

--- veebipood/src/main/java/ee/mihkel/veebipood/controller/OrderController.java
+++ veebipood/src/main/java/ee/mihkel/veebipood/controller/OrderController.java
@@ -1,6 +1,8 @@
 package ee.mihkel.veebipood.controller;
 
 import ee.mihkel.veebipood.dto.OrderRowDto;
+import ee.mihkel.veebipood.dto.ParcelMachine;
+import ee.mihkel.veebipood.dto.PaymentUrl;
 import ee.mihkel.veebipood.entity.Order;
 import ee.mihkel.veebipood.entity.OrderRow;
 import ee.mihkel.veebipood.repository.OrderRepository;
@@ -8,17 +10,30 @@ import ee.mihkel.veebipood.service.OrderService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class OrderController {
 
-    private OrderRepository orderRepository;
-    private OrderService orderService;
+    private final OrderRepository orderRepository;
+    private final OrderService orderService;
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping("parcelmachines")
+    public List<ParcelMachine> getParcelMachines(@RequestParam String country) {
+        String url = "https://www.omniva.ee/locations.json";
+        ParcelMachine[] response = restTemplate.exchange(url, HttpMethod.GET, null, ParcelMachine[].class).getBody();
+        return Arrays.stream(response)
+                .filter(e -> e.getA0_name().equals(country.toUpperCase()))
+                .toList();
+    }
 
 
     @GetMapping("orders")
@@ -40,5 +55,10 @@ public class OrderController {
                                 @RequestBody List<OrderRowDto> orderRows){
         return orderService.saveOrder(personId, parcelMachine, orderRows); // siin salvestab
         //return orderRepository.findAll(); // siin on uuenenud seis
+    }
+
+    @PostMapping("pay")
+    public PaymentUrl makePayment(@RequestParam Long orderId, double sum) {
+        return orderService.makePayment(orderId, sum);
     }
 }
