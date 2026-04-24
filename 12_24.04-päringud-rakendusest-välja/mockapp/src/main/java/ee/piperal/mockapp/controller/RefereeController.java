043b1326b297267a5deeee59728d81c8af5d3aab
piperal <piperal@tlu.ee>
2026-04-24 11:49:09 +0300

    updated to be simpler

--- 12_24.04-päringud-rakendusest-välja/mockapp/src/main/java/ee/piperal/mockapp/controller/RefereeController.java
+++ 12_24.04-päringud-rakendusest-välja/mockapp/src/main/java/ee/piperal/mockapp/controller/RefereeController.java
@@ -1,48 +1,27 @@
 package ee.piperal.mockapp.controller;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 
 
 @RestController
 public class RefereeController {
     @GetMapping("/kohtunikud")
-    public static String getRef() throws IOException {
-        StringBuilder result = new StringBuilder();
-        URL url = new URL("https://69e9ecab15c7e2d5126905f6.mockapi.io/sportlased/kohtunikud");
-        URLConnection con = url.openConnection();
-        con.setDoOutput(true);
-        try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(con.getInputStream()))) {
-            for (String line; (line = reader.readLine()) != null; ) {
-                result.append(line);
-            }
-        }
-        return result.toString();
+    public String getRef() throws IOException {
+        String url = "https://69e9ecab15c7e2d5126905f6.mockapi.io/sportlased/kohtunikud";
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.exchange(url, HttpMethod.GET, null, String.class).getBody();
     }
 
     @GetMapping("/asukohad")
-    public static String getLoc() throws IOException {
-        StringBuilder result = new StringBuilder();
-        URL url = new URL("https://69e9ecab15c7e2d5126905f6.mockapi.io/sportlased/asukohad");
-        URLConnection con = url.openConnection();
-        con.setDoOutput(true);
-        try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(con.getInputStream()))) {
-            for (String line; (line = reader.readLine()) != null; ) {
-                result.append(line);
-            }
-        }
-        return result.toString();
+    public String getLoc() throws IOException {
+        String url = "https://69e9ecab15c7e2d5126905f6.mockapi.io/sportlased/asukohad";
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.exchange(url, HttpMethod.GET, null, String.class).getBody();
     }
 }
